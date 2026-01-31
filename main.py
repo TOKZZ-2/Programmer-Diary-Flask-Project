2ffacfad4767799b17aec7595fe2a9b1fbd67c24
TOKZZ <122718056+TOKZZ-2@users.noreply.github.com>
2026-01-31 19:20:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 #Задаем секретный ключ для работы session
-app.secret_key = 'Sticker'
+app.secret_key = '&&&'
 #Подключение SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
